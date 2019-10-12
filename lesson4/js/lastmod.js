22ceca3aaba6a29396babda1d137a6220c76ef1e
santanatm.github.io <santanat@byui.edu>
2019-10-12 12:26:20 -0400

    update date

--- lesson4/js/lastmod.js
+++ lesson4/js/lastmod.js
@@ -1,2 +1,22 @@
-var lastUpdated = "Last Updated: " + document.lastModified;
-document.getElementById("lastMod").innerHTML=lastUpdated;
+function GetCurrentYear()
+{
+    var date = new Date();
+    var currentYear = date.getFullYear();
+
+    document.getElementById("copyrightdate").innerHTML = currentYear;
+}
+
+function GetCurrentDate()
+{
+    var date = new Date();
+
+    var dayOfWeek = date.toLocaleDateString("en-gb", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }); 
+
+    document.getElementById("currentdate").innerHTML = dayOfWeek;
+}
+
+function UpdateDates()
+{
+    GetCurrentYear();
+    GetCurrentDate();
+}
