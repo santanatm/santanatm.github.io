cc539126ac228edfbb9061fc1e66ccb6b1915e4f
santanatm.github.io <santanat@byui.edu>
2019-10-26 19:25:59 -0400

    getwindChill()

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,7 +2,7 @@
     var tF = parseInt(document.getElementById("tempF").innerHTML);
     var wS = parseInt(document.getElementById("windSpeed").innerHTML);
     if (tempF > 50 || windSpeed < 3){
-        document.getElementById("windchill").innerHTML = "0";}
+        document.getElementById("windchill").innerHTML = "N/A";}
     else {        
         var wC = 35.74 + (0.6215 * tF) - (35.75 * wS**0.16) + (0.4275 * tF * wS**0.16);
         document.getElementById("windchill").innerHTML = wC.toFixed(2) + ' °F';
