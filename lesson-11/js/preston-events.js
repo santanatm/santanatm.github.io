0182b216f9750635b280b757f0ce710366e969f6
santanatm.github.io <santanat@byui.edu>
2019-11-26 05:01:38 -0400

    events

---
+++ lesson-11/js/preston-events.js
@@ -0,0 +1,30 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+  .then(function (response)
+  {
+    return response.json();   
+  })
+    .then(function (jsonObject)
+  {
+    console.table(jsonObject);
+    const towndata = jsonObject['towns'];
+    for (let i = 0; i < towndata.length; i++ )
+    {
+        if (towndata[i].name != "Preston")
+        {
+            continue;
+        }
+        let townEventDiv = document.getElementById("town-events");
+        for (let j =0; j < towndata[i]['events'].length; j++)
+        {
+            let eventDetailsDiv = document.createElement('div');
+            var eventStringArray = towndata[i]['events'][j].split(':')
+            eventDetailsDiv.innerHTML = eventStringArray[0] + ":";
+            let eventDescriptionSpan = document.createElement('span');
+            eventDescriptionSpan.innerHTML = eventStringArray[1];
+            eventDetailsDiv.appendChild(eventDescriptionSpan);
+            eventDetailsDiv.className = "town-events-list-item";
+            townEventDiv.appendChild(eventDetailsDiv);
+        }
+    }
+  });
