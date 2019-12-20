c16de250aaef6e82de6fc042e9a4cec8b12bf817
santanatm.github.io <santanat@byui.edu>
2019-12-20 10:31:20 -0400

    temples.js

--- FinalProject/js/temples.js
+++ FinalProject/js/temples.js
@@ -39,22 +39,22 @@ fetch(requestURL, {
             image.setAttribute('alt', alttext);
             image.setAttribute('title', alttext);
 
-            services.textContent ='';
+            services.textContent ='Services:\n';
             for (let j = 0; j < temples[i].services.length; j++) {
                 services.textContent = services.textContent + temples[i].services[j] + '\n';
             }
 
-            ordinanceschedule.textContent ='';
+            ordinanceschedule.textContent ='Ordinances Schedule:\n';
             for (let k = 0; k < temples[i].ordinanceschedule.length; k++) {
                 ordinanceschedule.textContent = ordinanceschedule.textContent + temples[i].ordinanceschedule[k] + '\n';
             }
 
-            sessionschedule.textContent ='';
+            sessionschedule.textContent ='Sessions Schedule:\n';
             for (let l = 0; l < temples[i].sessionschedule.length; l++) {
                 sessionschedule.textContent = sessionschedule.textContent + temples[i].sessionschedule[l] + '\n';
             }
 
-            closureschedule.textContent ='';
+            closureschedule.textContent ='Closure Schedule\n';
             for (let m = 0; m < temples[i].closureschedule.length; m++) {
                 closureschedule.textContent = closureschedule.textContent + temples[i].closureschedule[m] + '\n';
             }
