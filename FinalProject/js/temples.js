0369c1aed66c8397148d522a75355c2b78e2a861
santanatm.github.io <santanat@byui.edu>
2019-12-20 07:10:09 -0400

    temples.js

--- FinalProject/js/temples.js
+++ FinalProject/js/temples.js
@@ -17,11 +17,6 @@ fetch(requestURL, {
             let MyTemple = document.createElement('section');
             let h2 = document.createElement('h2');
             let image = document.createElement('img');
-            let address1 = document.createElement('div');
-            let address2 = document.createElement('div');
-            let address3 = document.createElement('div');
-            let address4 = document.createElement('div');
-            let address = temples[i].address1 + temples[i].address2 + temples[i].address3 + temples[i].address4;
             let telephone = document.createElement('div');
             let email = document.createElement('div');
             let displayname = temples[i].displayname;
@@ -37,7 +32,6 @@ fetch(requestURL, {
 
             MyTemple.appendChild(h2);
             MyTemple.appendChild(image);
-            MyTemple.appendChild(address);
             MyTemple.appendChild(telephone);
             MyTemple.appendChild(email);
 
