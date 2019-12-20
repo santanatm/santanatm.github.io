9e5dc2cbcc2e1d87704561963caa16f38450a149
santanatm.github.io <santanat@byui.edu>
2019-12-20 06:52:39 -0400

    json

--- FinalProject/js/temples.js
+++ FinalProject/js/temples.js
@@ -16,11 +16,16 @@ fetch(requestURL, {
         for (let i = 0; i < temples.length; i++) {
             let MyTemple = document.createElement('section');
             let h2 = document.createElement('h2');
+            let image = document.createElement('img');
+            let address1 = document.createElement('div');
+            let address2 = document.createElement('div');
+            let address3 = document.createElement('div');
+            let address4 = document.createElement('div');
+            let address = temples[i].address1 + temples[i].address2 + temples[i].address3 + temples[i].address4;
             let telephone = document.createElement('div');
             let email = document.createElement('div');
-            let image = document.createElement('img');
             let displayname = temples[i].displayname;
-            let alttext = displayname + ' - ' + (i + 1);
+            let alttext = displayname;
 
             h2.textContent = displayname;
             temples.textContent = 'Telephone: ' + temples[i].telephone;
@@ -31,9 +36,11 @@ fetch(requestURL, {
             image.setAttribute('title', alttext);
 
             MyTemple.appendChild(h2);
+            MyTemple.appendChild(image);
+            MyTemple.appendChild(address);
             MyTemple.appendChild(telephone);
             MyTemple.appendChild(email);
-            MyTemple.appendChild(image);
+
 
             document.querySelector('div.MyTemples').appendChild(MyTemple);
         }
